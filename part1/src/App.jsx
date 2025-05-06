21d5b8c7462d88cd57a5599f4dacb2694933ec00
MyriamGo <myriam7489@gmail.com>
2025-05-06 20:33:20 +0200

    primer cambio mg

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,26 +1,40 @@
-const Hello = (props) => {
-
-  console.log(props)
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+const Content = (props) => {
   return (
     <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
+      <Parts parts={props.parts} exercises={props.exercises}/>
     </div>
+  )
+}
+const Parts = (props) => {
+  return (
+    <p>
+      {props.parts[0]} = {props.exercises[0]}<br />
+      {props.parts[1]} = {props.exercises[1]}<br />
+      {props.parts[2]} = {props.exercises[2]}<br />
+    </p>
+  )
+}
+const Total = (props) => {
+  return (
+    <p>Number of exercises = {props.exercises[0] + props.exercises[1] + props.exercises[2]}</p>
   )
 }
 
 const App = () => {
-
-  const name = 'Peter'
-  const age = 10
+  const course = 'Half Stack application development'
+  const parts = [ 'Fundamentals of React', 'Using props to pass data', 'State of a component']
+  const exercises = [10, 7, 14]
 
   return (
     <div>
-      <h1>Greetings</h1>
-
-      <Hello name='Maya' age={26 + 10} />
-      <Hello name={name} age={age} />
+      <Header course={course} />
+      <Content parts={parts} exercises={exercises} />
+      <Total exercises={exercises} />
     </div>
   )
 }
